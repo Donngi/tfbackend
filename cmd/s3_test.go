17bb92a44156b75ab884550d82a95c7a204c5d51
Jimon-s <22317839+Jimon-s@users.noreply.github.com>
2021-07-30 08:27:00 +0900

    Fix mockS3XXXXXAPIs to generate various mock function

--- cmd/s3_test.go
+++ cmd/s3_test.go
@@ -28,10 +28,11 @@ func Test_createBucket_Success(t *testing.T) {
 
 	// Given
 	tests := []struct {
-		name string
-		args args
-		api  func(t *testing.T) S3CreateBucketAPI
-		want string
+		name    string
+		args    args
+		api     func(t *testing.T) S3CreateBucketAPI
+		want    string
+		wantErr bool
 	}{
 		{
 			name: "S01: Happy path",
@@ -47,7 +48,8 @@ func Test_createBucket_Success(t *testing.T) {
 					return output, nil
 				})
 			},
-			want: "ap-northeast-1",
+			want:    "ap-northeast-1",
+			wantErr: false,
 		},
 	}
 
@@ -57,7 +59,7 @@ func Test_createBucket_Success(t *testing.T) {
 			got, err := createS3Bucket(context.Background(), tt.api(t), tt.args.bucketName, tt.args.region)
 
 			// Then
-			if err != nil {
+			if (err != nil) != tt.wantErr {
 				t.Errorf("createBucket() error = %v, want = nil", err)
 			}
 			if *got.Location != tt.want {
@@ -75,10 +77,11 @@ func Test_createBucket_Failure(t *testing.T) {
 
 	// Given
 	tests := []struct {
-		name string
-		args args
-		api  func(t *testing.T) S3CreateBucketAPI
-		want string
+		name    string
+		args    args
+		api     func(t *testing.T) S3CreateBucketAPI
+		want    string
+		wantErr bool
 	}{
 		{
 			name: "F01: Duplicated bucket name",
@@ -91,10 +94,11 @@ func Test_createBucket_Failure(t *testing.T) {
 					params *s3.CreateBucketInput,
 					optFns ...func(*s3.Options)) (*s3.CreateBucketOutput, error) {
 
-					return nil, errors.New("Duplicated error")
+					return nil, errors.New("duplicated error")
 				})
 			},
-			want: "failure💀",
+			want:    "failure💀",
+			wantErr: true,
 		},
 	}
 
@@ -104,8 +108,272 @@ func Test_createBucket_Failure(t *testing.T) {
 			_, err := createS3Bucket(context.Background(), tt.api(t), tt.args.bucketName, tt.args.region)
 
 			// Then
-			if err == nil {
+			if (err != nil) != tt.wantErr {
 				t.Errorf("createBucket() error = %v, want = some error", err)
+			}
+		})
+	}
+}
+
+type mockS3PutPublicAccessBlockAPI func(ctx context.Context,
+	params *s3.PutPublicAccessBlockInput,
+	optFns ...func(*s3.Options)) (*s3.PutPublicAccessBlockOutput, error)
+
+func (m mockS3PutPublicAccessBlockAPI) PutPublicAccessBlock(ctx context.Context,
+	params *s3.PutPublicAccessBlockInput,
+	optFns ...func(*s3.Options)) (*s3.PutPublicAccessBlockOutput, error) {
+
+	return m(ctx, params, optFns...)
+}
+
+func Test_enableAllPublicAccessBlock_Success(t *testing.T) {
+	type args struct {
+		bucketName string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		api     func(t *testing.T) S3PutPublicAccessBlockAPI
+		wantErr bool
+	}{
+		{
+			name: "S01: Happy path",
+			args: args{
+				bucketName: "happy-bucket",
+			},
+			api: func(t *testing.T) S3PutPublicAccessBlockAPI {
+				return mockS3PutPublicAccessBlockAPI(func(ctx context.Context,
+					params *s3.PutPublicAccessBlockInput,
+					optFns ...func(*s3.Options)) (*s3.PutPublicAccessBlockOutput, error) {
+
+					return &s3.PutPublicAccessBlockOutput{}, nil
+				})
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := enableAllPublicAccessBlock(context.Background(), tt.api(t), tt.args.bucketName)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("enableAllPublicAccessBlock() error = %v, wantErr = %v", err, tt.wantErr)
+				return
+			}
+			if got == nil {
+				t.Errorf("enableAllPublicAccessBlock() got = nil, want some object")
+			}
+		})
+	}
+}
+
+func Test_enableAllPublicAccessBlock_Failure(t *testing.T) {
+	type args struct {
+		bucketName string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		api     func(t *testing.T) S3PutPublicAccessBlockAPI
+		wantErr bool
+	}{
+		{
+			name: "F01: Bucket not exists",
+			args: args{
+				bucketName: "no-exist-bucket",
+			},
+			api: func(t *testing.T) S3PutPublicAccessBlockAPI {
+				return mockS3PutPublicAccessBlockAPI(func(ctx context.Context,
+					params *s3.PutPublicAccessBlockInput,
+					optFns ...func(*s3.Options)) (*s3.PutPublicAccessBlockOutput, error) {
+
+					return nil, errors.New("no exist error")
+				})
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := enableAllPublicAccessBlock(context.Background(), tt.api(t), tt.args.bucketName)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("enableAllPublicAccessBlock() error = %v, wantErr = %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
+
+type mockS3PutBucketEncryptionAPI func(ctx context.Context,
+	params *s3.PutBucketEncryptionInput,
+	optFns ...func(*s3.Options)) (*s3.PutBucketEncryptionOutput, error)
+
+func (m mockS3PutBucketEncryptionAPI) PutBucketEncryption(ctx context.Context,
+	params *s3.PutBucketEncryptionInput,
+	optFns ...func(*s3.Options)) (*s3.PutBucketEncryptionOutput, error) {
+
+	return m(ctx, params, optFns...)
+}
+
+func Test_enableBucketEncryptionAES256_Success(t *testing.T) {
+	type args struct {
+		bucketName string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		api     func(t *testing.T) mockS3PutBucketEncryptionAPI
+		wantErr bool
+	}{
+		{
+			name: "S01: Happy path",
+			args: args{
+				bucketName: "happy-bucket",
+			},
+			api: func(t *testing.T) mockS3PutBucketEncryptionAPI {
+				return mockS3PutBucketEncryptionAPI(func(ctx context.Context,
+					params *s3.PutBucketEncryptionInput,
+					optFns ...func(*s3.Options)) (*s3.PutBucketEncryptionOutput, error) {
+
+					return &s3.PutBucketEncryptionOutput{}, nil
+				})
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := enableBucketEncryptionAES256(context.Background(), tt.api(t), tt.args.bucketName)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("enableBucketEncryptionAES256() error = %v, wantErr = %v", err, tt.wantErr)
+				return
+			}
+			if got == nil {
+				t.Errorf("enableBucketEncryptionAES256() got = nil, want some object")
+			}
+		})
+	}
+}
+
+func Test_enableBucketEncryptionAES256_Failure(t *testing.T) {
+	type args struct {
+		bucketName string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		api     func(t *testing.T) mockS3PutBucketEncryptionAPI
+		wantErr bool
+	}{
+		{
+			name: "F01: Bucket not exists",
+			args: args{
+				bucketName: "no-exist-bucket",
+			},
+			api: func(t *testing.T) mockS3PutBucketEncryptionAPI {
+				return mockS3PutBucketEncryptionAPI(func(ctx context.Context,
+					params *s3.PutBucketEncryptionInput,
+					optFns ...func(*s3.Options)) (*s3.PutBucketEncryptionOutput, error) {
+
+					return nil, errors.New("no exist error")
+				})
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := enableBucketEncryptionAES256(context.Background(), tt.api(t), tt.args.bucketName)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("enableBucketEncryptionAES256() error = %v, wantErr = %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
+
+type mockS3PutBucketVersioningAPI func(ctx context.Context,
+	params *s3.PutBucketVersioningInput,
+	optFns ...func(*s3.Options)) (*s3.PutBucketVersioningOutput, error)
+
+func (m mockS3PutBucketVersioningAPI) PutBucketVersioning(ctx context.Context,
+	params *s3.PutBucketVersioningInput,
+	optFns ...func(*s3.Options)) (*s3.PutBucketVersioningOutput, error) {
+
+	return m(ctx, params, optFns...)
+}
+
+func Test_enableBucketVersioning_Success(t *testing.T) {
+	type args struct {
+		bucketName string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		api     func(t *testing.T) S3PutBucketVersioningAPI
+		wantErr bool
+	}{
+		{
+			name: "S01: Happy path",
+			args: args{
+				bucketName: "happy-bucket",
+			},
+			api: func(t *testing.T) S3PutBucketVersioningAPI {
+				return mockS3PutBucketVersioningAPI(func(ctx context.Context,
+					params *s3.PutBucketVersioningInput,
+					optFns ...func(*s3.Options)) (*s3.PutBucketVersioningOutput, error) {
+
+					return &s3.PutBucketVersioningOutput{}, nil
+				})
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := enableBucketVersioning(context.Background(), tt.api(t), tt.args.bucketName)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("enableBucketVersioning() error = %v, wantErr = %v", err, tt.wantErr)
+				return
+			}
+			if got == nil {
+				t.Errorf("enableBucketVersioning() got = nil, want some object")
+			}
+		})
+	}
+}
+
+func Test_enableBucketVersioning_Failure(t *testing.T) {
+	type args struct {
+		bucketName string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		api     func(t *testing.T) S3PutBucketVersioningAPI
+		wantErr bool
+	}{
+		{
+			name: "F01: Bucket not exists",
+			args: args{
+				bucketName: "no-exist-bucket",
+			},
+			api: func(t *testing.T) S3PutBucketVersioningAPI {
+				return mockS3PutBucketVersioningAPI(func(ctx context.Context,
+					params *s3.PutBucketVersioningInput,
+					optFns ...func(*s3.Options)) (*s3.PutBucketVersioningOutput, error) {
+
+					return nil, errors.New("no exist error")
+				})
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := enableBucketVersioning(context.Background(), tt.api(t), tt.args.bucketName)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("enableBucketVersioning() error = %v, wantErr = %v", err, tt.wantErr)
+				return
 			}
 		})
 	}
