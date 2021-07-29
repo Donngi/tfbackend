37a0fa9ce57747fa9aa196d76a94fa00efdf5618
Jimon-s <22317839+Jimon-s@users.noreply.github.com>
2021-07-30 07:49:45 +0900

    Fix mockS3CreateBucketAPI to generate various mock function

--- cmd/s3_test.go
+++ cmd/s3_test.go
@@ -2,22 +2,22 @@ package cmd
 
 import (
 	"context"
+	"errors"
 	"testing"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
-type mockCreateBucketAPI struct{}
+type mockS3CreateBucketAPI func(ctx context.Context,
+	params *s3.CreateBucketInput,
+	optFns ...func(*s3.Options)) (*s3.CreateBucketOutput, error)
 
-func (m mockCreateBucketAPI) CreateBucket(ctx context.Context,
+func (m mockS3CreateBucketAPI) CreateBucket(ctx context.Context,
 	params *s3.CreateBucketInput,
 	optFns ...func(*s3.Options)) (*s3.CreateBucketOutput, error) {
 
-	output := &s3.CreateBucketOutput{
-		Location: aws.String("ap-northeast-1"),
-	}
-	return output, nil
+	return m(ctx, params, optFns...)
 }
 
 func Test_createBucket_Success(t *testing.T) {
@@ -30,11 +30,23 @@ func Test_createBucket_Success(t *testing.T) {
 	tests := []struct {
 		name string
 		args args
+		api  func(t *testing.T) S3CreateBucketAPI
 		want string
 	}{
 		{
 			name: "S01: Happy path",
 			args: args{bucketName: "sample-bucket", region: "ap-northeast-1"},
+			api: func(t *testing.T) S3CreateBucketAPI {
+				return mockS3CreateBucketAPI(func(ctx context.Context,
+					params *s3.CreateBucketInput,
+					optFns ...func(*s3.Options)) (*s3.CreateBucketOutput, error) {
+
+					output := &s3.CreateBucketOutput{
+						Location: aws.String("ap-northeast-1"),
+					}
+					return output, nil
+				})
+			},
 			want: "ap-northeast-1",
 		},
 	}
@@ -42,8 +54,7 @@ func Test_createBucket_Success(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			// When
-			api := mockCreateBucketAPI{}
-			got, err := createS3Bucket(context.Background(), api, tt.args.bucketName, tt.args.region)
+			got, err := createS3Bucket(context.Background(), tt.api(t), tt.args.bucketName, tt.args.region)
 
 			// Then
 			if err != nil {
@@ -56,35 +67,46 @@ func Test_createBucket_Success(t *testing.T) {
 	}
 }
 
-// TODO
-// func Test_createBucket_Failure(t *testing.T) {
-// 	type args struct {
-// 		bucketName string
-// 	}
+func Test_createBucket_Failure(t *testing.T) {
+	type args struct {
+		bucketName string
+		region     string
+	}
 
-// 	// Given
-// 	tests := []struct {
-// 		name string
-// 		args args
-// 		want string
-// 	}{
-// 		{
-// 			name: "F01: Duplicated bucket name",
-// 			args: args{bucketName: "duplicated-bucket"},
-// 			want: "failure💀",
-// 		},
-// 	}
+	// Given
+	tests := []struct {
+		name string
+		args args
+		api  func(t *testing.T) S3CreateBucketAPI
+		want string
+	}{
+		{
+			name: "F01: Duplicated bucket name",
+			args: args{
+				bucketName: "duplicated-bucket",
+				region:     "ap-northeast-1",
+			},
+			api: func(t *testing.T) S3CreateBucketAPI {
+				return mockS3CreateBucketAPI(func(ctx context.Context,
+					params *s3.CreateBucketInput,
+					optFns ...func(*s3.Options)) (*s3.CreateBucketOutput, error) {
 
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			// When
-// 			api := mockCreateBucketAPI{}
-// 			_, err := createS3Bucket(context.Background(), api, tt.args.bucketName)
+					return nil, errors.New("Duplicated error")
+				})
+			},
+			want: "failure💀",
+		},
+	}
 
-// 			// Then
-// 			if err == nil {
-// 				t.Errorf("createBucket() error = %v, want = some error", err)
-// 			}
-// 		})
-// 	}
-// }
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// When
+			_, err := createS3Bucket(context.Background(), tt.api(t), tt.args.bucketName, tt.args.region)
+
+			// Then
+			if err == nil {
+				t.Errorf("createBucket() error = %v, want = some error", err)
+			}
+		})
+	}
+}
